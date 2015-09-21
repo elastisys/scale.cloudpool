fe9c01dce982a3018c65bee5c9644447acb432a7
petergardfjall <peter.gardfjall.work@gmail.com>
2015-09-21 15:39:58 +0200

    make log output dir configurable

--- aws/spot/src/main/docker/opt/elastisys/spotpool/start.sh
+++ aws/spot/src/main/docker/opt/elastisys/spotpool/start.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
+set -e
+
 # detect java location
 JAVA_HOME="$(readlink -f /usr/bin/java | sed "s:/jre/bin/java::")"
 java="${JAVA_HOME}/bin/java"
 
+# where log output files are written
+if [ "${LOG_DIR}" = "" ]; then
+    LOG_DIR="/var/log/elastisys/spotpool"
+fi
+
+# where runtime state is kept
+if [ "${STORAGE_DIR}" = "" ]; then
+    STORAGE_DIR="/var/lib/elastisys/spotpool"
+fi
+
+
 logconfig="/etc/elastisys/spotpool/logback.xml"
 julconfig="/etc/elastisys/spotpool/logging.properties"
 security_dir="/etc/elastisys/security"
-storage_dir="/var/lib/elastisys/spotpool"
 httpsport=443
 
 # Java system properties
 JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djava.util.logging.config.file=${julconfig} -Dlogback.configurationFile=${logconfig}"
+JAVA_OPTS="${JAVA_OPTS} -DLOG_DIR=${LOG_DIR}"
+
 SECURITY_OPTS="--ssl-keystore ${security_dir}/server_keystore.p12 --ssl-keystore-password serverpassword"
 
 # require clients to authenticate with a trusted certificate
 # SECURITY_OPTS="${SERVER_OPTS} --ssl-truststore ${security_dir}/server_truststore.jks --ssl-truststore-password serverpassword --require-cert"
 
-SERVER_OPTS="--storage-dir ${storage_dir} --https-port ${httpsport} ${SECURITY_OPTS}"
+SERVER_OPTS="--storage-dir ${STORAGE_DIR} --https-port ${httpsport} ${SECURITY_OPTS}"
 
 # to allow use of ports below 1024, use: authbind --deep <prog> [args ...]
 # note: assumes that /etc/authbind/byport/<port> files with proper owner/mode
