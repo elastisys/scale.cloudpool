32e43a9929b257be3ed552834b8e5a6c3b71bfe1
petergardfjall <peter.gardfjall.work@gmail.com>
2015-10-06 14:10:04 +0200

    updated to aws sdk version 1.10.22. removed redundant aws request classes.

--- aws/commons/src/main/java/com/elastisys/scale/cloudpool/aws/commons/requests/autoscaling/AmazonAutoScalingRequest.java
+++ aws/commons/src/main/java/com/elastisys/scale/cloudpool/aws/commons/requests/autoscaling/AmazonAutoScalingRequest.java
@@ -3,13 +3,12 @@ package com.elastisys.scale.cloudpool.aws.commons.requests.autoscaling;
 import com.amazonaws.auth.AWSCredentials;
 import com.elastisys.scale.cloudpool.aws.commons.client.AutoScalingApiClient;
 import com.elastisys.scale.cloudpool.aws.commons.requests.AmazonRequest;
-import com.elastisys.scale.cloudpool.aws.commons.requests.elb.AmazonElbRequest;
 
 /**
  * An abstract base class for AWS Auto Scaling request clients.
- * 
- * 
- * 
+ *
+ *
+ *
  * @param <R>
  *            The response type.
  */
@@ -18,8 +17,8 @@ public abstract class AmazonAutoScalingRequest<R> extends AmazonRequest<R> {
 	private final AutoScalingApiClient client;
 
 	/**
-	 * Constructs a new {@link AmazonElbRequest}.
-	 * 
+	 * Constructs a new {@link AmazonAutoScalingRequest}.
+	 *
 	 * @param awsCredentials
 	 *            AWS security credentials for the account to be used.
 	 * @param region
@@ -32,7 +31,7 @@ public abstract class AmazonAutoScalingRequest<R> extends AmazonRequest<R> {
 
 	/**
 	 * Returns the AWS Auto Scaling API client.
-	 * 
+	 *
 	 * @return
 	 */
 	public AutoScalingApiClient getClient() {
