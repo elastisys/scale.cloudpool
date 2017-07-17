9c8d967cd999ab98a366f8b5e9a3509272c9aa5d
Jakob Lindqvist <jakoblindqvist1990@gmail.com>
2017-07-17 16:50:05 +0200

    removed private constructor from Tag

--- vsphere/src/main/java/com/elastisys/scale/cloudpool/vsphere/client/tag/Tag.java
+++ vsphere/src/main/java/com/elastisys/scale/cloudpool/vsphere/client/tag/Tag.java
@@ -4,8 +4,6 @@ import java.util.HashSet;
 
 public final class Tag {
 
-    private Tag () {}
-
     public static final String CLOUD_POOL = "elastisys:cloudPool";
     public static final String SERVICE_STATE = "elastisys:serviceState";
     public static final String MEMBERSHIP_STATUS = "elastisys:membershipStatus";
